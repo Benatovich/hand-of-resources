aee7ae5b60a822dd4c57e9c6a23ff95aaee65982
Benatovich <sambenatovich@gmail.com>
2022-03-18 19:35:18 -0400

    create attribute functionality complete, test passes

--- __tests__/attributes.test.js
+++ __tests__/attributes.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Attribute = require('../lib/models/Attribute');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -11,4 +12,21 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('creates a new entry in the attributes table', async () => {
+    const res = await request(app)
+      .post('/api/v1/attributes')
+      .send({
+        name: 'vigor',
+        stats: 'hp, defense'
+      });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'vigor',
+      stats: 'hp, defense'
+    });
+  });
+
+
 });

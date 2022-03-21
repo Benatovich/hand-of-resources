7ab33125909e1807f744da74f9eb8e3b4feeb088
Benatovich <sambenatovich@gmail.com>
2022-03-21 15:11:27 -0400

    first weapon test passes

--- __tests__/weapons.test.js
+++ __tests__/weapons.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Weapon = require('../lib/models/Weapon');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -11,4 +12,20 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('creates a new entry in the weapons table', async () => {
+    const res = await request(app)
+      .post('/api/v1/weapons')
+      .send({
+        name: 'battleaxe',
+        usable: 1
+      });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'battleaxe',
+      usable: true
+    });
+  });
+
 });

2c3a5936348f1808cd0bc836e0bc4a9ecaca13fa
Benatovich <sambenatovich@gmail.com>
2022-03-21 14:56:00 -0400

    first spells test passes

--- __tests__/spells.test.js
+++ __tests__/spells.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Spell = require('../lib/models/Spell');
 
 describe('hand-of-resources routes', () => {
   beforeEach(() => {
@@ -11,4 +12,20 @@ describe('hand-of-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('creates a new entry in the spells table', async () => {
+    const res = await request(app)
+      .post('/api/v1/spells')
+      .send({
+        name: 'magic missile',
+        usable: 1
+      });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'magic missile',
+      usable: true
+    });
+  });
+
 });
